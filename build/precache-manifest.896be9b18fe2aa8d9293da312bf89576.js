e53601cf0bb23562335d74a8f8ff56b885dec77e
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2024-07-16 21:09:07 +0530

    Add files via upload

---
+++ build/precache-manifest.896be9b18fe2aa8d9293da312bf89576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba7e9bd92a2f97f4a353a8ae37bf15b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2684003dfe8b5e65a420",
+    "url": "/static/css/main.ca87938a.chunk.css"
+  },
+  {
+    "revision": "9cbe13e10c9f113485c1",
+    "url": "/static/js/2.e96df388.chunk.js"
+  },
+  {
+    "revision": "2684003dfe8b5e65a420",
+    "url": "/static/js/main.1f681d65.chunk.js"
+  },
+  {
+    "revision": "f1b768174fa530eb5144",
+    "url": "/static/js/runtime-main.39dd3371.js"
+  }
+]);
